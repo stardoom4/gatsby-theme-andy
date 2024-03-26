99fd87707ee7f6f0683af7a5b70cd640aa6d8d09
Celestial-Entity <139852159+stardoom4@users.noreply.github.com>
2024-03-26 12:24:27 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,6 @@
-const path = require('path');
-
-module.exports = ({
-  notesDirectory = 'content/',
-  notesFileExtensions = ['.md', '.mdx'],
-  noteTemplate = path.join(__dirname, 'src/templates/note.js'),
-  additionalNoteTypes = {},
-  rootPath = '/',
-  rootNote = 'hello',
-  linkifyHashtags = false,
-  hideDoubleBrackets = false,
-  mdxOtherwiseConfigured = false,
-  themeUIOtherwiseConfigured = false,
-  generateRSS = false,
-  rssPath = '/rss.xml',
-  rssTitle = 'My Notes',
-}) => ({
+module.exports = {
   siteMetadata: {
-    title: 'Clesstial Entity',
+    title: 'Celestial Entity',
   },
-  plugins: [
-    {
-      resolve: '@aengusm/gatsby-theme-brain',
-      options: {
-        notesDirectory,
-        notesFileExtensions,
-        noteTemplate,
-        additionalNoteTypes,
-        rootPath,
-        rootNote,
-        linkifyHashtags,
-        hideDoubleBrackets,
-        mdxOtherwiseConfigured,
-        generateRSS,
-        rssPath,
-        rssTitle,
-      },
-    },
-    !themeUIOtherwiseConfigured && `gatsby-plugin-theme-ui`,
-  ],
-});
+  plugins: [`gatsby-theme-andy`],
+};
